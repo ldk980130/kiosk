83bfa733312cdffe683222d7dc534646af4c8219
DK <ldk980130@gmail.com>
2021-11-25 22:17:28 +0900

    feat :Item 생성자, 비즈니스 로직 구현

--- src/main/java/com/postick/kiosk/domain/Item.java
+++ src/main/java/com/postick/kiosk/domain/Item.java
@@ -32,4 +32,39 @@ public class Item {
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "category_id")
 	private Category category;
+
+	public static Item create(Category category, String name, int price, int stockQuantity) {
+
+		Item item = new Item();
+
+		item.name = name;
+		item.price = price;
+		item.stockQuantity = stockQuantity;
+
+		item.category = category;
+		category.getItems().add(item);
+
+		return item;
+	}
+
+	/**
+	 * stock 추가
+	 */
+	public void addStock(int quantity) {
+		this.stockQuantity += quantity;
+	}
+
+	/**
+	 * stock 감소
+	 */
+	public void removeStock(int quantity) {
+
+		int restStock = this.stockQuantity - quantity;
+
+		if (restStock < 0) {
+			throw new IllegalStateException("need more stock");
+		}
+
+		this.stockQuantity = restStock;
+	}
 }
