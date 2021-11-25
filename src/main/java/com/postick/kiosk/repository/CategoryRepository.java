c2668de2eb651752e32a1edd347f3cd6ff018fd2
DK <ldk980130@gmail.com>
2021-11-25 23:49:56 +0900

    feat : CategoryRepository 전체 조회 기능 추가

--- src/main/java/com/postick/kiosk/repository/CategoryRepository.java
+++ src/main/java/com/postick/kiosk/repository/CategoryRepository.java
@@ -1,5 +1,7 @@
 package com.postick.kiosk.repository;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 
 import org.springframework.stereotype.Repository;
@@ -17,5 +19,10 @@ public class CategoryRepository {
 	public Long save(Category category) {
 		em.persist(category);
 		return category.getId();
+	}
+
+	public List<Category> findAll() {
+		return em.createQuery("select c from Category c", Category.class)
+			.getResultList();
 	}
 }
