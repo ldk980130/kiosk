967cf5b4c8a38993cfdcfab712ecd392e0784c5c
DK <ldk980130@gmail.com>
2021-11-25 14:44:13 +0900

    feat : Item 객체 필드 정의

---
+++ src/main/java/com/postick/kiosk/domain/Item.java
@@ -0,0 +1,27 @@
+package com.postick.kiosk.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Item {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private Long price;
+
+	@Column(nullable = false)
+	private Long stockQuantity;
+}
