e35e90f7403648359fe0e95dca2d2eefca815abd
DK <ldk980130@gmail.com>
2021-11-25 15:06:34 +0900

    feat : OrderItem - Item 연관관계 매핑

--- src/main/java/com/postick/kiosk/domain/OrderItem.java
+++ src/main/java/com/postick/kiosk/domain/OrderItem.java
@@ -2,9 +2,12 @@ package com.postick.kiosk.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 import lombok.Getter;
 
@@ -22,4 +25,8 @@ public class OrderItem {
 
 	@Column(nullable = false)
 	private Integer count;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "item_id")
+	private Item item;
 }
