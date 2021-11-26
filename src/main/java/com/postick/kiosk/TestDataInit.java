690dfe24c2b3a83cc79537b067d8330e64010d4b
DK <ldk980130@gmail.com>
2021-11-26 21:29:30 +0900

    feat : 테스트 데이터 디비에 입력

--- src/main/java/com/postick/kiosk/TestDataInit.java
+++ src/main/java/com/postick/kiosk/TestDataInit.java
@@ -1,27 +1,61 @@
-package com.postick.kiosk;
-
-import javax.annotation.PostConstruct;
-
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.postick.kiosk.domain.Category;
-import com.postick.kiosk.repository.CategoryRepository;
-
-import lombok.RequiredArgsConstructor;
-
-@Component
-@RequiredArgsConstructor
-public class TestDataInit {
-
-	private final CategoryRepository categoryRepository;
-
-	@PostConstruct
-	public void dataInit() {
-
-		categoryRepository.save(Category.create("커피"));
-		categoryRepository.save(Category.create("차"));
-		categoryRepository.save(Category.create("스무디"));
-		categoryRepository.save(Category.create("디저트"));
-	}
-}
+// package com.postick.kiosk;
+//
+// import java.util.ArrayList;
+// import java.util.List;
+//
+// import javax.annotation.PostConstruct;
+//
+// import org.springframework.stereotype.Component;
+//
+// import com.postick.kiosk.domain.Category;
+// import com.postick.kiosk.domain.Item;
+// import com.postick.kiosk.domain.OrderItem;
+// import com.postick.kiosk.repository.CategoryRepository;
+// import com.postick.kiosk.repository.ItemRepository;
+// import com.postick.kiosk.repository.OrderRepository;
+//
+// import lombok.RequiredArgsConstructor;
+//
+// @Component
+// @RequiredArgsConstructor
+// public class TestDataInit {
+//
+// 	private final CategoryRepository categoryRepository;
+// 	private final ItemRepository itemRepository;
+// 	private final OrderRepository orderRepository;
+//
+// 	static String COFFEE = "커피";
+// 	static String TEA = "차";
+// 	static String SMOOTHIE = "스무디";
+// 	static String DESSERT = "디저트";
+//
+// 	@PostConstruct
+// 	public void dataInit() {
+//
+// 		Category coffee = categoryRepository.save(COFFEE);
+// 		Category tea = categoryRepository.save(TEA);
+// 		Category smoothie = categoryRepository.save(SMOOTHIE);
+// 		Category dessert = categoryRepository.save(DESSERT);
+//
+// 		Item americano = itemRepository.save(coffee, "아메리카노", 3000);
+// 		Item cafeLatte = itemRepository.save(coffee, "카페라떼", 3500);
+// 		Item citron = itemRepository.save(tea, "유자차", 3300);
+// 		Item greenTea = itemRepository.save(tea, "녹차", 3300);
+// 		Item berrySmoothie = itemRepository.save(smoothie, "딸기 스무디", 4500);
+// 		Item yogurtSmoothie = itemRepository.save(smoothie, "요거트 스무디", 4300);
+// 		Item cake = itemRepository.save(dessert, "케이크", 5500);
+// 		Item croffle = itemRepository.save(dessert, "크로플", 5000);
+//
+// 		List<OrderItem> list = new ArrayList<>();
+// 		list.add(OrderItem.create(americano, 2));
+// 		list.add(OrderItem.create(cafeLatte, 1));
+// 		list.add(OrderItem.create(citron, 1));
+// 		list.add(OrderItem.create(greenTea, 1));
+// 		list.add(OrderItem.create(berrySmoothie, 2));
+// 		list.add(OrderItem.create(yogurtSmoothie, 1));
+// 		list.add(OrderItem.create(cake, 1));
+// 		list.add(OrderItem.create(croffle, 1));
+//
+// 		orderRepository.save(list);
+// 	}
+// }
