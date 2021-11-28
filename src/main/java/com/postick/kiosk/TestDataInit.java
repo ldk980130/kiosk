420832d14b0ad3e0b3b8e3adf670a61f563e5d5f
DK <ldk980130@gmail.com>
2021-11-29 01:30:38 +0900

    chore : 테스트 데이터 수정

--- src/main/java/com/postick/kiosk/TestDataInit.java
+++ src/main/java/com/postick/kiosk/TestDataInit.java
@@ -26,29 +26,38 @@ public class TestDataInit {
 
 	static String COFFEE = "커피";
 	static String TEA = "차";
-	static String SMOOTHIE = "스무디";
-	static String DESSERT = "디저트";
+	static String JUICE = "주스";
+	static String BAKERY = "빵";
 
 	@PostConstruct
 	public void dataInit() {
 
 		Category coffee = categoryRepository.save(COFFEE);
 		Category tea = categoryRepository.save(TEA);
-		Category smoothie = categoryRepository.save(SMOOTHIE);
-		Category dessert = categoryRepository.save(DESSERT);
+		Category juice = categoryRepository.save(JUICE);
+		Category bakery = categoryRepository.save(BAKERY);
 
-		Item americano = itemRepository.save(coffee, "아메리카노", 3000);
-		Item cafeLatte = itemRepository.save(coffee, "카페라떼", 3500);
+		Item americanoH = itemRepository.save(coffee, "아메리카노(HOT)", 3700);
+		Item americanoI = itemRepository.save(coffee, "아메리카노(ICE)", 3700);
+		Item cafeLatteH = itemRepository.save(coffee, "카페라떼(HOT)", 4000);
+		Item cafeLatteI = itemRepository.save(coffee, "카페라떼(ICE)", 4000);
+		Item cafeMochaH = itemRepository.save(coffee, "카페모카(HOT)", 4300);
+		Item cafeMochaI = itemRepository.save(coffee, "카페모카(ICE)", 4300);
+		Item hazelnutH = itemRepository.save(coffee, "헤이즐넛 라떼(HOT)", 4300);
+		Item hazelnutI = itemRepository.save(coffee, "헤이즐넛 라떼(ICE)", 4300);
+
 		Item citron = itemRepository.save(tea, "유자차", 3300);
 		Item greenTea = itemRepository.save(tea, "녹차", 3300);
-		Item berrySmoothie = itemRepository.save(smoothie, "딸기 스무디", 4500);
-		Item yogurtSmoothie = itemRepository.save(smoothie, "요거트 스무디", 4300);
-		Item cake = itemRepository.save(dessert, "케이크", 5500);
-		Item croffle = itemRepository.save(dessert, "크로플", 5000);
+
+		Item berrySmoothie = itemRepository.save(juice, "딸기 스무디", 4500);
+		Item yogurtSmoothie = itemRepository.save(juice, "요거트 스무디", 4300);
+
+		Item cake = itemRepository.save(bakery, "케이크", 5500);
+		Item croffle = itemRepository.save(bakery, "크로플", 5000);
 
 		List<OrderItem> list = new ArrayList<>();
-		list.add(OrderItem.create(americano, 2));
-		list.add(OrderItem.create(cafeLatte, 1));
+		list.add(OrderItem.create(americanoH, 2));
+		list.add(OrderItem.create(cafeLatteI, 1));
 		list.add(OrderItem.create(citron, 1));
 		list.add(OrderItem.create(greenTea, 1));
 		list.add(OrderItem.create(berrySmoothie, 2));
