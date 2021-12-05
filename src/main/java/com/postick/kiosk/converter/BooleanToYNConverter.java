4b0ca0a44c36af205e18519b7e3d2451883c8988
DK <ldk980130@gmail.com>
2021-12-05 18:38:46 +0900

    fix : 테이크아웃 여부 컨버터 Y, N에서 Yes, No로 변경

--- src/main/java/com/postick/kiosk/converter/BooleanToYNConverter.java
+++ src/main/java/com/postick/kiosk/converter/BooleanToYNConverter.java
@@ -13,20 +13,20 @@ public class BooleanToYNConverter implements AttributeConverter<Boolean, String>
 	 * Boolean 값을 Y 또는 N으로 컨버트
 	 *
 	 * @param attribute boolean 값
-	 * @return String true인 경우 Y, false인 경우 N
+	 * @return String true인 경우 Yes, false인 경우 No
 	 */
 	@Override
 	public String convertToDatabaseColumn(Boolean attribute) {
-		return (attribute != null && attribute) ? "Y" : "N";
+		return (attribute != null && attribute) ? "Yes" : "No";
 	}
 
 	/**
 	 * Y 또는 N을 Boolean으로 컨버트
-	 * @param dbData Y 또는 N
-	 * @return boolean 대소문자 상관 없이 Y인 경우 true, N인 경우 false
+	 * @param dbData Yes 또는 No
+	 * @return boolean 대소문자 상관 없이 Yes인 경우 true, No인 경우 false
 	 */
 	@Override
 	public Boolean convertToEntityAttribute(String dbData) {
-		return "Y".equalsIgnoreCase(dbData);
+		return "Yes".equalsIgnoreCase(dbData);
 	}
 }
